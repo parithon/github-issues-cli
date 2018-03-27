196c78b5b56077c162ac97a40edc764c2ff4269f
Jerrie Pelser <jerrie@jerriepelser.com>
2018-03-27 18:48:52 +0700

    Create basic app template

---
+++ src/GitHubIssuesCli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GitHubIssuesCli
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            return 0;
+        }
+    }
+}
