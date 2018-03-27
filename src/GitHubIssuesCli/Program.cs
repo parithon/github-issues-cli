17021c06a75ff0f860b6c545f5a5d6db646232db
Jerrie Pelser <jerrie@jerriepelser.com>
2018-03-27 20:20:12 +0700

    Basic command line arguments in place

--- src/GitHubIssuesCli/Program.cs
+++ src/GitHubIssuesCli/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace GitHubIssuesCli
 {
+    [Command(Name = "ghi", Description = "A GitHub Issues client")]
+    [Subcommand("auth", typeof(AuthCommand))]
+    [HelpOption]
     class Program
     {
-        static async Task<int> Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static Task<int> Main(string[] args) => CommandLineApplication.ExecuteAsync<Program>(args);
 
-            return 0;
+        private Task<int> OnExecuteAsync(CommandLineApplication app)
+        {
+            app.ShowHelp();
+            
+            return Task.FromResult(0);
+        }
+    }
+
+    [Command(Description = "Authenticates a user with GitHub")]
+    [HelpOption]
+    class AuthCommand
+    {
+        [Argument(0,  Description = "The GitHub Personal Access Token to use")]
+        public string Token { get; }
+
+        private Task<int> OnExecuteAsync(IConsole console)
+        {
+            // See https://github.com/googlesamples/oauth-apps-for-windows/blob/master/OAuthConsoleApp/OAuthConsoleApp/Program.cs
+            
+            return Task.FromResult(0);
         }
     }
 }
+
