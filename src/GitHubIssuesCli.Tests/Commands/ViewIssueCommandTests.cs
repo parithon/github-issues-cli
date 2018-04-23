56c6f728bb397a6d4ecdd8340feda8930a34e656
Jerrie Pelser <jerrie@jerriepelser.com>
2018-04-23 12:17:26 +0700

    Add more view tests

--- src/GitHubIssuesCli.Tests/Commands/ViewIssueCommandTests.cs
+++ src/GitHubIssuesCli.Tests/Commands/ViewIssueCommandTests.cs
@@ -12,34 +12,87 @@ namespace GitHubIssuesCli.Tests.Commands
 {
     public class ViewIssueCommandTests
     {
+        private readonly Mock<IGitHubClient> _gitHubClient;
+        private readonly Mock<IGitHubRepositoryDiscoveryService> _discoveryService;
+        private readonly Mock<IBrowserService> _browserService;
+        private readonly Mock<IReporter> _reporter;
+        private readonly Mock<IConsole> _console;
+
+        private const string Owner = "jerriep";
+        private const string Repo = "github-issues-cli";
+        private const int IssueNumber = 100;
+        
+        public ViewIssueCommandTests()
+        {
+            _gitHubClient = new Mock<IGitHubClient>();
+            _discoveryService = new Mock<IGitHubRepositoryDiscoveryService>();
+            _browserService = new Mock<IBrowserService>();
+            _reporter = new Mock<IReporter>();
+            _console = new Mock<IConsole>();
+        }
+        
+        [Fact]
+        public async Task NotInARepoFolder_ReportsError()
+        {
+            _discoveryService.Setup(service => service.DiscoverInCurrentDirectory())
+                .Returns(() => null);
+
+            ViewIssueCommand command = new ViewIssueCommand(_gitHubClient.Object, _discoveryService.Object, _browserService.Object, _reporter.Object);
+            command.Issue = $"{IssueNumber}";
+            await command.OnExecuteAsync(_console.Object);
+
+            _reporter.Verify(r => r.Error(It.IsAny<string>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task NotInARepoFolder_OpensBrowser_WhenFullIssueSpecified()
+        {
+            var issuesClient = new Mock<IIssuesClient>();
+            var repositoriesClient = new Mock<IRepositoriesClient>();
+
+            repositoriesClient.Setup(client => client.Get(Owner, Repo))
+                .Returns(Task.FromResult(GitHubModelFactory.CreateRepository(Owner, Repo)));
+            issuesClient.Setup(client => client.Get(Owner, Repo, IssueNumber))
+                .Returns(Task.FromResult(GitHubModelFactory.CreateIssue(Owner, Repo, IssueNumber)));
+
+            _gitHubClient.Setup(client => client.Issue)
+                .Returns(issuesClient.Object);
+            _gitHubClient.Setup(client => client.Repository)
+                .Returns(repositoriesClient.Object);
+            _discoveryService.Setup(service => service.DiscoverInCurrentDirectory())
+                .Returns(() => new GitHubRepositoryInfo(Owner, Repo));
+
+            ViewIssueCommand command = new ViewIssueCommand(_gitHubClient.Object, _discoveryService.Object, _browserService.Object, _reporter.Object);
+            command.Issue = $"{Owner}/{Repo}#{IssueNumber}";
+            await command.OnExecuteAsync(_console.Object);
+
+            _browserService.Verify(service => service.OpenBrowser($"https://github.com/{Owner}/{Repo}/issues/{IssueNumber}"), Times.Once);
+        }
+
         [Fact]
         public async Task ViewValidIssue_OpensBrowser()
         {
-            var gitHubClient = new Mock<IGitHubClient>();
             var issuesClient = new Mock<IIssuesClient>();
             var repositoriesClient = new Mock<IRepositoriesClient>();
-            var discoveryService = new Mock<IGitHubRepositoryDiscoveryService>();
-            var browserService = new Mock<IBrowserService>();
-            var reporter = new Mock<IReporter>();
-            var console = new Mock<IConsole>();
 
-            gitHubClient.Setup(client => client.Issue)
+            repositoriesClient.Setup(client => client.Get(Owner, Repo))
+                .Returns(Task.FromResult(GitHubModelFactory.CreateRepository(Owner, Repo)));
+            issuesClient.Setup(client => client.Get(Owner, Repo, IssueNumber))
+                .Returns(Task.FromResult(GitHubModelFactory.CreateIssue(Owner, Repo, IssueNumber)));
+
+            _gitHubClient.Setup(client => client.Issue)
                 .Returns(issuesClient.Object);
-            gitHubClient.Setup(client => client.Repository)
+            _gitHubClient.Setup(client => client.Repository)
                 .Returns(repositoriesClient.Object);
-            repositoriesClient.Setup(client => client.Get("jerriep", "github-issues-cli"))
-                .Returns(Task.FromResult(GitHubModelFactory.CreateRepository("jerriep", "github-issues-cli")));
-            issuesClient.Setup(client => client.Get("jerriep", "github-issues-cli", 100))
-                .Returns(Task.FromResult(GitHubModelFactory.CreateIssue("jerriep", "github-issues-cli", 100)));
-            discoveryService.Setup(service => service.DiscoverInCurrentDirectory())
-                .Returns(() => new GitHubRepositoryInfo("jerriep", "github-issues-cli"));
+            _discoveryService.Setup(service => service.DiscoverInCurrentDirectory())
+                .Returns(() => new GitHubRepositoryInfo(Owner, Repo));
             
-
-            ViewIssueCommand command = new ViewIssueCommand(gitHubClient.Object, discoveryService.Object, browserService.Object, reporter.Object);
-            command.Issue = "100";
-            await command.OnExecuteAsync(console.Object);
+            ViewIssueCommand command = new ViewIssueCommand(_gitHubClient.Object, _discoveryService.Object, _browserService.Object, _reporter.Object);
+            command.Issue = $"{IssueNumber}";
+            await command.OnExecuteAsync(_console.Object);
             
-            browserService.Verify(service => service.OpenBrowser("https://github.com/jerriep/github-issues-cli/issues/100"), Times.Once);
+            _browserService.Verify(service => service.OpenBrowser($"https://github.com/{Owner}/{Repo}/issues/{IssueNumber}"), Times.Once);
         }
+
     }
 }
